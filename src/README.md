3a5d02b762a60dbae514f35f4c170043cfb57cbb
yangtaoProject <yangtaolalala@gmail.com>
2019-08-10 17:28:08 +0800

    change

--- src/README.md
+++ src/README.md
@@ -1,17 +1,11 @@
-#代码的说明文件
+#代码的说明文件：
 
 #https://www.zhihu.com/question/19568896 主流开源协议之间的异同
-
-
-
-#用命令行把代码提交到Github
-1、git add .
-2、git commit -m "提交信息"
-3、git push origin master
 
 
 #制作首页App组件
 1、完成Header区域，使用的是Mint-UI中的Header组件
 2、制作底部的Tabbar区域，使用的是muui中的Tabbar组件
-啦啦啦啦
-lalalall
+   2.1  先把扩展图标的css样式拷贝到项目中，根据错误提示导入相应的扩展包
+3、要在中间区域放置一个router-view来展示路由匹配到的组件
+4、创建路由实例对象后，把tabbar的a标签改成router-link标签
--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
 import Vue from '../node_modules/vue/dist/vue'      //这是完整的vue包
-
+// 导入路由的包
+import VuerRouter from 'vue-router'
+Vue.use(VuerRouter)
 
 import './lib/mui/css/mui.css'
+import './lib/mui/css/icons-extra.css'
 
 //导入Mint-UI包和样式
 import MintUI from 'mint-ui'
@@ -10,11 +13,23 @@ Vue.use(MintUI)
 
 
 import app from './app.vue'   //导入app组件
+
+// 定义路由对象
+var router = new VuerRouter({
+    routes:[
+
+    ],
+    linkActiveClass:'mui-active'   //覆盖默认的路由高亮的类，默认的类叫做router-link-active
+})
+
 var vm = new Vue({
     el:'#root',
     data:{
 
     },
-    render:c=>c(app)
-   
+    render:c=>c(app),
+    router    //一定记住在vue实例上挂载路有对象   
 })
+
+
+
